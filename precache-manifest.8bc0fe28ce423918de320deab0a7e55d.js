a533b04af6485a3c34fecc6590d4cf20c25c4614
VICTOR MELO BARBERO <0015080@insags.com>
2020-03-29 10:12:00 +0200

    Updates

---
+++ precache-manifest.8bc0fe28ce423918de320deab0a7e55d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d77cca1eb6e215b0e06362022b536d9",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "92b166926de672ce1d12",
+    "url": "/my-app/static/js/2.053a9a9d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.053a9a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c4e06901cec77eb8cde",
+    "url": "/my-app/static/js/main.ea8d0ffc.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  }
+]);
